67fb0a468c614c323fbf8fd1d72946e8f1344848
akira-zip <jrgtf.space@gmail.com>
2025-03-13 14:35:23 -0300

    Update index.py

--- func-calc/index.py
+++ func-calc/index.py
@@ -1,26 +1,27 @@
-def Operacoes(Opcao, number1, number2):
+def Operacoes(Opcao, Number1, Number2):
   match Opcao:
     case 1:
-      return(number1 + number2)
+      return(Number1 + Number2)
     case 2:
-      return(number1 - number2)
+      return(Number1 - Number2)
     case 3:
-      return(number1 * number2)
+      return(Number1 * Number2)
     case 4:
-      return(number1 / number2)
+      return(Number1 / Number2)
     
 def Main():
   Restart = 0
-  Data =[]
+  Data = []
 
   for i in range(0, 2):
     Data.append(float(input('Digite o ' + str(i + 1) + '° número \n')))
 
   Data.append(int(input('Escolha uma operação: Somar(1), Subtrair(2), Multiplicar(3), Dividir(4) \n')))
 
-  if((Data[2] == 4) and (Data[0] or Data[1] == 0)):
-    print('Não é possível dividir por zero! Tente novamente')
-    Main()
+  if(Data[2] == 4):
+    if(Data[0] == 0 or Data[1] == 0):
+      print('Não é possível dividir por zero! Tente novamente')
+      Main()
 
   print(Operacoes(Data[2], Data[0], Data[1]))
   Restart = int(input('Escolha se deseja continuar: Sim(1), Não(2) \n'))

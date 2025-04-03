3350a4291efcfceaf4be0616a352036cbfe82ea7
akira-zip <jrgtf.space@gmail.com>
2025-04-03 11:05:01 -0300

    Update index.py

--- func-calc/index.py
+++ func-calc/index.py
@@ -1,3 +1,6 @@
+restart = 0
+data = [0, 0, 0]
+
 def operacoes(opcao, n1, n2):
   match opcao:
     case 1:
@@ -15,14 +18,26 @@ def operacoes(opcao, n1, n2):
     case 7:
       return(n1 % n2)
     
+def getData(opcao):
+  match opcao:
+    case 1:
+      try:
+        for i in range(0, 2):
+          data[i] = (float(input('Digite o ' + str(i + 1) + '° número \n')))
+      except:
+        print('Tente novamente')
+        getData(1)
+    case 2:
+      try:
+        data[2] = int(input('Digite os valores para as respectivas operações: \n[1] - Somar \n[2] - Subtrair \n[3] - Multiplicar \n[4] - Dividir \n[5] - Exponenciação \n[6] - Divisão inteira \n[7] - Módulo \nSelec: '))
+      except:
+        print('Tente novamente')
+        getData(2)
+
+    
 def main():
-  restart = 0
-  data = []
-
-  for i in range(0, 2):
-    data.append(float(input('Digite o ' + str(i + 1) + '° número \n')))
-
-  data.append(int(input('Digite os valores para as respectivas operações: \n[1] - Somar \n[2] - Subtrair \n[3] - Multiplicar \n[4] - Dividir \n[5] - Exponenciação \n[6] - Divisão inteira \n[7] - Módulo \nSelec: ')))
+  getData(1)
+  getData(2)
 
   if(data[2] == 4 or data[2] == 6 or data[2] == 7):
     if(data[0] == 0 or data[1] == 0):
